c6dbb23426dcc98eb8edfeec5e519be2c9c5e14e
ASUS <kallayandatta@gmail.com>
2026-01-15 00:21:10 +0600

    Solution 2 Added: Read 2 Integer and display Substract result

---
+++ Problem Solving/Operators/02_Read_Two_Integer_Display_Substract.rs
@@ -0,0 +1,26 @@
+use std::io;
+fn read_integer() -> i32 {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    match input.trim().parse::<i32>() {
+        Ok(value) => value,
+        Err(_) => {
+            println!("Invalid integer input");
+            std::process::exit(1);
+        }
+    }
+}
+
+fn main(){
+    println!("Enter the first integer:");
+    let num1 = read_integer();
+
+    println!("Enter the second integer:");
+    let num2 = read_integer();
+    
+    let subtract = num1 - num2;
+    println!("The subtraction of {} and {} is {}", num1, num2, subtract);
+
+}
