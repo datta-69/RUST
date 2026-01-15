11b5af45e0469d9656ecf06bd2abbd5cfbe931ff
ASUS <kallayandatta@gmail.com>
2026-01-15 17:08:15 +0600

    Solution 8 added on operators: It read temperature on celcious and display temperature in fahrenheit

---
+++ Problem Solving/Operators/08_Read_Celcious_Display_Fahrenheit.rs
@@ -0,0 +1,21 @@
+use std::io;
+fn read_temperature() -> f32 {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    match input.trim().parse::<f32>() {
+        Ok(value) => value,
+        Err(_) => {
+            println!("Invalid float input");
+            std::process::exit(1);
+        }
+    }
+}
+
+fn main(){
+    println!("Enter the temperature in Celsius:");
+    let celsius = read_temperature();
+    let fahrenheit = (celsius * 9.0/5.0) + 32.0;
+    println!("{} Celsius is equal to {} Fahrenheit", celsius, fahrenheit);
+}
