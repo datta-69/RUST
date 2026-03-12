45468bab3833cc8059c32564a7fa17652f4f86be
ASUS <kallayandatta@gmail.com>
2026-03-13 03:24:24 +0600

    Solution 15 & 16 added that read a number and find mod using BITWISE AND Operator

---
+++ Problem Solving Basics/Operators/16_Read_Number_Mod_By_8_Bitwise_AND.rs
@@ -0,0 +1,21 @@
+use std::io;
+fn read_number() -> i32 {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    match input.trim().parse::<i32>() {
+        Ok(value) => value,
+        Err(_) => {
+            println!("Invalid unsigned integer input");
+            std::process::exit(1);
+        }
+    }
+}
+
+fn main(){
+    println!("Enter a Number: ");
+    let number = read_number();
+    let remainder = number & 7; // Bitwise AND with 7 (binary 111)
+    println!("The remainder when {} is divided by 8 is: {}", number, remainder);
+}
